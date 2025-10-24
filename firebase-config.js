e14e38465f7cbfd9625b1d514461bb2e4bf5b5bd
art group of companies <artleasing2025@gmail.com>
2025-10-24 10:56:00 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Replace the object below with your Firebase project's config.
+// Get this from Project Settings -> General -> Your apps (Web)
+export const firebaseConfig = {
+  apiKey: "AIzaSyCOcsL9ahdFUunWDAcv5JZdnMxMZehquhg",
+    authDomain: "art-staffmanage.firebaseapp.com",
+    projectId: "art-staffmanage",
+    storageBucket: "art-staffmanage.firebasestorage.app",
+    messagingSenderId: "951269778971",
+    appId: "1:951269778971:web:11fbb26385588ced133abe"
+};
